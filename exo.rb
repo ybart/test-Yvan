83113d38c2f7a6cb57f069640535037cb1915cf9
Sam 'hide' Ettahiri <hide@cock.li>
2017-05-10 10:11:37 +0200

    Fin de l'entretient

--- exo.rb
+++ exo.rb
@@ -1,48 +1,22 @@
+def bs_quality_appreciation(item)
+  return 3 if item.sell_in <= 5
+  return 2 if item.sell_in <= 10
+  return -item.quality if item.sell_in < 0
+  return 1
+end
+
+def quality_appreciation(item)
+  return 0 if item.name == 'Sulfuras' || item.quality >= 50
+  return 1 if item.name == 'Aged Brie'
+  return -2 if item.sell_in <= 0
+  return bs_quality_appreciation(item) if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+  -1
+end
+
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-    end
+    item.quality += quality_appreciation(item)
+    item.sell_in -= 1
   end
 end
 
@@ -60,4 +34,3 @@ Item = Struct.new(:name, :sell_in, :quality)
 #   Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
 #   Item.new("Conjured Mana Cake", 3, 6),
 # ]
-
